c5384790a1d8e62fd34fa1adf5387a5940b1e8df
ty-omkarhs <hsomkar@yahoo.com>
2021-05-26 00:27:14 +0530

    all over

--- colorchange/src/App.js
+++ colorchange/src/App.js
@@ -1,35 +1,17 @@
 import "./App.css";
 
-class temp
-{
-   state = {
-    color: ""
-  };
-  
-   handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("handle Submit");
-    console.log(this.state);
-    document.body.style.backgroundColor = event.target.value;
-    this.setState({
-      color: ""
-    });
-  };
-  
-   handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
-}
+let handle = (event) => {
+  document.body.style.backgroundColor = document.getElementById("color").value;
+};
 
 function App() {
   return (
     <div>
-      <form onSubmit={temp.handleSubmit}>
-      <label for="color">Color: </label
-    ><input type="text" name="color" size="20" id="color"/>
-    <button type="submit" onChange={temp.handleChange}>Submit</button></form>
+      <label for="color">Color: </label>
+      <input type="text" name="color" size="20" id="color" />
+      <button type="button" onClick={handle}>
+        Submit
+      </button>
     </div>
   );
 }
